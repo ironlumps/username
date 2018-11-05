32e8ee7ccca3fe8d6f4961cc601d100755bdadaf
ironlumps <ryan.kassab@att.net>
2018-11-05 10:43:54 -0800

    Fixed username.sh

--- username.sh
+++ username.sh
@@ -2,7 +2,7 @@
 # username.sh
 echo "Enter a username: "
 read USER
-while echo $USER | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo $USER | egrep -v "^(([a-z])([a-z0-9_]){2,12})$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username - 3-12 characters and must begin with a lower case character!"
 	echo "Valid characters include: lower case letters, digits, and the underscore character"
