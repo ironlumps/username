def70624b80527ce91d7d9413966b95f0a2a9ce4
ironlumps <ryan.kassab@att.net>
2018-11-05 10:34:41 -0800

    Adding username.sh

---
+++ username.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# username.sh
+echo "Enter a username: "
+read USER
+while echo $USER | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username - 3-12 characters and must begin with a lower case character!"
+	echo "Valid characters include: lower case letters, digits, and the underscore character"
+	echo "Enter a username: "
+	read USER
+done
+echo "Thank you"
+
